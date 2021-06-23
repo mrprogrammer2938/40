6eea728144f8b4406ab975f0ff8aee2abbb3ecc1
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-23 10:02:51 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# This code write by Mr.nope
+clear
+echo "Installing..."
+sleep 2
+chmod +x 40
+sudo apt install python
+sudo apt install python3
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+echo "Usage: python3 40"
+echo ""
+exit 1
---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# This code write by Mr.nope
+# Uninstalling 40 Tool
+clear
+echo "Uninstalling..."
+sleep 2
+cd .. && sudo rm -r 40
+echo ""
+echo "Uninstalling..., Finish...!"
+echo ""
+exit 1
